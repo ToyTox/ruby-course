3020907999425d8e3c42233bcf1cb05a86ab152a
Artem Kosulin <artem.kosulin@flant.com>
2026-02-25 23:32:07 +0300

    Add reservation seats logic

--- Lesson-7/passenger_wagon.rb
+++ Lesson-7/passenger_wagon.rb
@@ -1,5 +1,25 @@
 class PassengerWagon < Wagon
-  def initialize
+  attr_reader seat_count
+
+  def initialize(seat_count)
     super(:passenger)
+    @seat_count = seat_count
+    @seats = []
+    @reserved_seat = 1
+  end
+
+  def reservation_seat
+    return if @seats.size > @seat_count
+
+    seats << @reserved_seat
+    sefl.reserved_seat += 1
+  end
+
+  def show_reserved_seat
+    @seats
+  end
+
+  def show_free_seat
+    @seat_count - (@seats.size + 1)
   end
 end
