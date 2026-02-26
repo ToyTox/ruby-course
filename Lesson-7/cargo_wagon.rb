5ecfbd00c620d352deea6a63bd2588c4bdbb10ae
Artem Kosulin <artem.kosulin@flant.com>
2026-02-26 22:19:16 +0300

    Add take the volume for the cargo wagon

--- Lesson-7/cargo_wagon.rb
+++ Lesson-7/cargo_wagon.rb
@@ -1,5 +1,23 @@
 class CargoWagon < Wagon
-  def initialize
+  attr_reader volume
+
+  def initialize(volume)
     super(:cargo)
+    @volume = volume
+    @total_taken_volume = 0
+  end
+
+  def take_volume(count)
+    return if count > @volume
+
+    sefl.total_taken_volume += count
+  end
+
+  def total_taken_volume
+    @total_taken_volume
+  end
+
+  def remaining_volume
+    @volume - @total_taken_volume
   end
 end
