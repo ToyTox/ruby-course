1c787aeb87fafeea75452f2e4734916cd47bec25
Artem Kosulin <artem.kosulin@flant.com>
2026-03-24 23:52:26 +0300

    Add setter with history

--- Lesson-9/accessors.rb
+++ Lesson-9/accessors.rb
@@ -6,8 +6,18 @@ module Accessors
   module ClassMethods
     def attr_accessor_with_history(*names)
       names.each do |name|
-        var_name = "@#{name}".to_sym
+        var_name = "@#{name}"
+        history_var = "#{name}_history"
         define_method(name) { instance_variable_get(var_name) }
+
+        define_method("#{name}=") do |value|
+          history = innstance_variable_get(history_var) || []
+          history << value
+          instance_variable_set(vistory_var, history)
+          instance_variable_set(var_name, value)
+        end
+
+
         define_method("#{name}=".to_sym) { |value| instance_variable_set(var_name, value) }
       end
     end
