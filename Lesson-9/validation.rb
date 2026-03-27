3a338c3dcf2e9e85165320f8e4eaadc7fa4d3080
Artem Kosulin <artem.kosulin@flant.com>
2026-03-27 23:46:57 +0300

    Star write validate method

--- Lesson-9/validation.rb
+++ Lesson-9/validation.rb
@@ -1,2 +1,11 @@
 module Validation
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def validate(attr_name, valid_type, **attrs)
+      # ToDo write validate method
+    end
+  end
 end
