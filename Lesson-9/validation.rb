ffdf1fe24af5accac9adf733de5d16eec411a1b6
Artem Kosulin <artem.kosulin@flant.com>
2026-04-01 23:56:56 +0300

    Add InstanceMethods module

--- Lesson-9/validation.rb
+++ Lesson-9/validation.rb
@@ -1,7 +1,7 @@
 module Validation
   def self.included(base)
     base.extend(ClassMethods)
-    base.send include, InstanceMethods
+    base.send(:include, InstanceMethods)
   end
 
   module ClassMethods
@@ -15,5 +15,22 @@ module Validation
   end
 
   module InstanceMethods
+    def validate!
+      return if self.class.attrs.nil?
+
+      self.class.attrs.each do |attr|
+        arg_name = instance_variable_get("@#{attr[:name]}")
+        send("validate_#{attr[:type]}", arg_name, *attr[:args])
+      end
+    end
+
+    def valid?
+      validate!
+      true
+    rescue StandardError
+      false
+    end
+
+    protected
   end
 end
